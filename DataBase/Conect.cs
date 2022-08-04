31e40e1a8f7f6723c998a2888508d9f024c79aab
Guilherm Consentino <guiconsentino@hotmail.com>
2022-08-04 19:09:36 -0300

    banco de dados conectado

---
+++ DataBase/Conect.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace ApiGuilherme.DataBase
+{
+    public class Conect
+    {
+
+        public Conect()
+        {
+            
+        }
+
+        public MySqlConnection conectar()
+        {
+            try
+            {
+                MySqlConnection con = new MySqlConnection();
+                con.ConnectionString = "server=localhost; database=teste; user id =root; password =****** port =3306;";
+                con.Open();
+                Console.WriteLine("deu certo");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return conectar();
+        }
+        
+    
+    }
+        
+    
+}
